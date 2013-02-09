4f0bc0be6485f80e3b5fa6b3c0eb4e1c7041a534
Vicente Oscar Mier Vela <vomv1988@gmail.com>
2013-02-09 14:23:43 -0600

    nop nop unnecessary in mv_w_tx

--- src/lib/mv_w_tx.asm
+++ src/lib/mv_w_tx.asm
@@ -8,9 +8,6 @@ mv_w_tx:
 	banksel		TXREG		;;	<< Move the contents of WREG to TXREG
 	movwf		TXREG		;;	<<
 
-	nop				;;	<< Wait for TRMT to be cleared
-	nop				;;	<<
-
 	banksel		TXSTA		;;	<< Pause execution until TRMT (TXSTA,1)
 	btfss		TXSTA,1		;;	<< is set again. i.e. until the byte
 	goto		$-1		;;	<< transmission completes
